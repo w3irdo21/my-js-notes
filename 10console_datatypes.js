92d4acbe4f59883ae47f38eaf922e019f1d187b5
Aastik <aastikgakhar01@gmail.com>
2023-03-02 07:28:41 +0530

    Revised data types and console API

---
+++ 10console_datatypes.js
@@ -0,0 +1,33 @@
+//  Ways to print in JavaScript
+console.log('Hello World',843+42789, '-: Answer here');
+document.write('Used Document Write here');
+alert('Attention here !');
+
+ //  JavaScript Console API 
+console.warn('used console warn here');
+console.error('Console error used here');
+console.assert(aas==aas);
+
+// Data types in JavaScript
+
+//  Numbers
+var num1=44;
+var num2=56;
+console.log(num1+num2);
+
+// Strings
+var str1="That's a String";
+var str2='This is also a string';
+console.log(str1);
+console.log(str2);
+console.log(str1 +str2);
+console.log(str1,str2);
+console.log(str1,str2, num1,num2);
+
+// Objects
+var details={
+    nm:'w3irdo_21',
+    age:20,
+    role:'full stack developer'
+}
+console.log(details)
